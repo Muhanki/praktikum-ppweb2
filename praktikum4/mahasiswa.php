6ccdc0d0401a470a251c902a8905ac0b71d68d2d
Muhanki <raihanqisti14@gmail.com>
2025-03-22 22:43:10 +0700

    Add files via upload

---
+++ praktikum4/mahasiswa.php
@@ -0,0 +1,18 @@
+<?php
+
+class Mahasiswa {
+    public $nama;
+    public $nim;
+    public $semester;
+
+    public function __construct() {
+        echo 'Mahasiswa baru telah dibuat! <br/>';
+    }
+    public function sayangsayangan() {
+        echo $nama 'Mahasiswa telah dihapus! <br/>';
+}
+$siti = new Mahasiswa();
+$siti->nama = 'Siti munaroh';
+
+$tono = new Mahasiswa();
+$tono->nama = 'Tono sukirman';
